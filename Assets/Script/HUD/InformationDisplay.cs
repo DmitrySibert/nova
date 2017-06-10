0e8999d9b43b0cd9c2a3b6529bad06d0502e5a03
Dmitry Sibert <dimaaasik.s@gmail.com>
2017-06-10 19:07:03 +0600

    Added HUD with viewing information about next comet and current scores.

---
+++ Assets/Script/HUD/InformationDisplay.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class InformationDisplay : MonoBehaviour {
+
+    [SerializeField]
+    private GameObject[] displayObjects;
+    [SerializeField]
+    private string[] objectTextNames;
+    [SerializeField]
+    private Text nextSpawnObjName;
+
+    private Dispatcher dispatcher;
+    private Dictionary<GameObject, string> objectNames;
+
+	private void Start ()
+    {
+        dispatcher = GetComponent<Dispatcher>();
+        objectNames = new Dictionary<GameObject, string>();
+        for(uint i = 0; i < displayObjects.Length; ++i) {
+            objectNames[displayObjects[i]] = objectTextNames[i];
+        }
+    }
+	
+	private void Update ()
+    {
+        Event evt = dispatcher.ReceiveEvent();
+        if (evt.Name.Equals("NextSpawn")) {
+            GameObject spawn = evt.Data.Get<GameObject>("SpawnGameObject");
+            string name = objectNames[spawn];
+            nextSpawnObjName.text = objectNames[spawn];
+        }
+	}
+}
